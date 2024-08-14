ea16b2d983fae7cfb5e5c5f517feb3782db3c31c
gt-min <gt_min@naver.com>
2024-08-14 17:18:51 +0900

    f

--- src/main/java/com/min/springjpa/SpringjpaApplication.java
+++ src/main/java/com/min/springjpa/SpringjpaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)  // 데이터베이스 설정이 없어도 빌드가 가능하도록 설정
 @ComponentScan(basePackages = "com.min.springjpa")
 public class SpringjpaApplication {
 
