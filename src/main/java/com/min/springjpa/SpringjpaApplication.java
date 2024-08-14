31ad7bc15f121bad3b9d6cdedc5bcbc568935651
gt-min <gt_min@naver.com>
2024-08-14 17:11:09 +0900

    d

--- src/main/java/com/min/springjpa/SpringjpaApplication.java
+++ src/main/java/com/min/springjpa/SpringjpaApplication.java
@@ -3,8 +3,10 @@ package com.min.springjpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = "com.min.springjpa")
 public class SpringjpaApplication {
 
   public static void main(String[] args) {
